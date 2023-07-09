e5571ef22250db8cf9f8b642189e79e3160ab5e7
Oliver <Oliver.suda@seznam.cz>
2023-07-09 18:40:37 +0200

    4

--- IN_PROGRESS/Interactive map/script.js
+++ IN_PROGRESS/Interactive map/script.js
@@ -40,9 +40,8 @@ e6e6d:0x8e51b7118f4e2207!5m2!4m1!1i2!8m2!3d49.7250938!4d13.3272072!15sCgRp
 YmlzWgYiBGliaXOSAQVob3RlbJoB
 JENoZERTVWhOTUc5blMwVkpRMEZuU1VSek9FbHBPVEpCUlJBQuABAA!16s%2Fg%2F1vq9k_mz`
 
-let ibisText = `This is
-<strong><a href="https://www.hotelibisplzen.cz/en/" target="_blank">hotel IBIS</a></strong> and those countries
-    are located here. You can find this place on
+let ibisText = `Link for the wepage of the hotel
+<strong><a href="https://www.hotelibisplzen.cz/en/" target="_blank">here</a></strong>. You can find this place on
     adress <strong><a href="${googleMap}" target="_blank">Univerzitní 65</a></strong>.`
 
     const markerIbis = L.marker([49.72509145010219, 13.327203611886718]).addTo(map);
@@ -59,9 +58,11 @@ y+Marriott+Pilsen/@49.7485401,13.3812928,19.25z/data=!4m13!1m2!2m
 i2!8m2!3d49.748559!4d13.3815618!15sCghtYXJyaW90dCI
 DiAEBWgoiCG1hcnJpb3R0kgEFaG90ZWzgAQA!16s%2Fg%2F1hc41rx4h`
 
-let marriottText = `This is
-<strong><a target="_blank" href="https://www.marriott.com/en-us/hotels/prgpz-courtyard-pilsen/overview/?scid=00bac83c-e2ae-47ac-a320-a6f8f82bb36d&gclid=CjwKCAjw3POhBhBQEiwAqTCuBpxfE3rIrC5UaPAOVoTIQ9J2ARepAv2VCHD9vcyoOyrVbvt5hlgFKRoCnzwQAvD_BwE&gclsrc=aw.ds">hotel Marriott</a></strong> and those countries
-    are located here. You can find this place on
+let marriottText = `Link for the wepage of the hotel
+<strong><a target="_blank" href="https://www.marriott.com/en-us/hotels/prgpz-courtyard-p
+ilsen/overview/?scid=00bac83c-e2ae-47ac-a320-a6f8f82bb36d&gclid=CjwKCAjw3POhBhBQEiwAqTCuB
+pxfE3rIrC5UaPAOVoTIQ9J2ARepAv2VCHD9vcyoOyrVbvt5hlgFKRoCnzwQAvD_BwE&gclsrc=aw.d
+s">here</a></strong>. Headquarters will be located on this hotel. You can find this place on
     adress <strong><a href="${googleMap}" target="_blank">sady 5. května 57</a></strong>.`
 const markerMariott = L.marker([49.74855314066368, 13.381562916491971]).addTo(map);
 markerMariott.bindPopup(template("hotel Marriott", marriottText,"pictures/marriott.png"));
@@ -75,9 +76,10 @@ googleMap = `https://www.google.com/maps/place/Vienna+House+Easy
 m8!1s0x470af1e7b5332fa1:0x8bc9bbb49a5133b1!5m2!4m1!1i2!8m2!3d49.7
 458415!4d13.3869324!16s%2Fg%2F1w_vl_3y`
 
-let viennaText = `This is
-<strong><a target="_blank" href="https://www.guestreservations.com/angelo-by-vienna-house-pilsen/booking?gclid=CjwKCAjw3POhBhBQEiwAqTCuBo78Cy6cr2A61Dtn2VveHzvC1MxilXKOdAZfAphf2so9sSJEPoWIvBoCRLAQAvD_BwE">hotel Vienna House</a></strong> and those countries
-    are located here. You can find this place on
+let viennaText = `Link for the wepage of the hotel
+<strong><a target="_blank" href="https://www.guestreservations.com/angelo-by-vienna-house
+-pilsen/booking?gclid=CjwKCAjw3POhBhBQEiwAqTCuBo78Cy6cr2A61Dtn2VveHzvC1MxilXKOdAZfAphf2so
+9sSJEPoWIvBoCRLAQAvD_BwE">here</a></strong>. You can find this place on
     adress <strong><a href="${googleMap}" target="_blank">U Prazdroje 2720</a></strong>.`
 const markerVienna = L.marker([49.74582909117203, 13.386963975377626]).addTo(map);
 markerVienna.bindPopup(template("Vienna House", viennaText,"pictures/vienna_house.jpg"));
@@ -93,10 +95,10 @@ e%C5%88/@49.7470529,13.3762436,17.75z/data
 e4daee!8m2!3d49.7467733!4d13.3777404!16zL20vMGd4NGZm`
 
 let namestiText = `This is
-<strong>republic Square</strong> and there will be final stage of the tournament placed. This place on is
-    adress <strong><a href="${googleMap}" target="_blank">301 00 Plzeň 3</a></strong>.`
+<strong>Republic Square</strong> and the finals of the tournames will be held here.
+    Adress is <strong><a href="${googleMap}" target="_blank">301 00 Plzeň 3</a></strong>.`
 const markerNamesti = L.marker([49.74699695171426, 13.37747555603402]).addTo(map);
-markerNamesti.bindPopup(template('Republic square', namestiText,"pictures/namesti.jpg"));
+markerNamesti.bindPopup(template('Final archery range<br><span class="date">22nd and 23rd July</span>', namestiText,"pictures/namesti.jpg"));
 
 
 
@@ -106,7 +108,22 @@ googleMap = `https://www.google.com/maps/place/Fotbalov%C3%BD+Stadi
 on+Lu%C4%8Dn%C3%AD/@49.7548941,13.3886487,18z/data=!4m6!3m5!1s0x470a
 f1eb0e45eb15:0xbd73355f05a89153!8m2!3d49.7553673!4d13.3902556!16s%2Fg%2F11fx913t5q`
 
-let lucniText = `On this football field will be held knockout stage of the tournament. You can find it on adress
+let lucniText = `First stage of the tournamet will be held here. You can find it on adress
 <strong><a href="${googleMap}" target="_blank">Luční 645/54</a></strong>.`
-const markerLucni = L.marker([49.75460761416636, 13.389108020761466]).addTo(map);
-markerLucni.bindPopup(template('football stadium<br>"Lucni"', lucniText,"pictures/lucni.jpg"));
+const markerLucni = L.marker([49.757859, 13.388880]).addTo(map);
+markerLucni.bindPopup(template('1st archery range<br><span class="date">17th-21st July</span>', lucniText,"pictures/lucni.jpg"));
+
+// catering Lucni
+let cateringText = ``
+const markerCatering = L.marker([49.756813, 13.390129]).addTo(map);
+markerCatering.bindPopup(template('Catering', cateringText,"pictures/lucni.jpg"));
+
+//equipment storage
+let equipmentText = ``
+const markerEquipment = L.marker([49.756565, 13.390530]).addTo(map); 
+markerEquipment.bindPopup(template('Equipment storage', equipmentText,"pictures/lucni.jpg"));
+
+// roots
+let polygon = L.polygon([
+    [51.509, -0.08],
+]).addTo(map);
